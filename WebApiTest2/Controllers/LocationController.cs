97934d0b0011c87ebb46fc0e024a64e7709e02ec
PragyaVyas <120946186+PragyaVyas@users.noreply.github.com>
2023-10-29 18:16:50 +1000

    Solution : 2

---
+++ WebApiTest2/Controllers/LocationController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using Intercom.Data;
+
+namespace WebApiTest2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        private const string ApiKey = "fe923f99dc9706382322810f1740c060\r\n";
+        private const string IpStackApiUrl = "http://api.ipstack.com/";
+        [HttpGet]
+        public async Task<IActionResult> GetLocationByIP(string ipAddress)
+        {
+            using var client = new HttpClient(); client.BaseAddress = new Uri(IpStackApiUrl);
+            try
+            {   
+                // Build the URL with the IP address and API key
+                 string url = $"{ipAddress}?access_key={ApiKey}";
+
+                // Send a GET request to the IP/Location API 
+                var response = await client.GetFromJsonAsync<LocationData>(url);
+
+                if (response != null) { return Ok(response); } 
+                else { return BadRequest("Unable to retrieve location information."); }
+            }
+            catch (Exception ex) { return StatusCode(500, ex.Message); }
+        }
+    }
+}
---
+++ WebApiTest2/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
+namespace WebApiTest2
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public void Configure(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
